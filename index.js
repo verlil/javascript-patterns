ce0b95a70916eaf6ae4c699f114e48f3e05c5789
name <mail>
2019-05-15 16:33:43 +0300

    Abstract factory

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
 class Car {
-    constructor(doors, engine, color) {
+    constructor(doors, engine, color, model) {
         this.doors = doors;
         this.engine = engine;
         this.color = color;
-
+        this.model = model;
     }
 }
 
-class carFactory {
+class CarFactory {
     createCar(type) {
         switch (type) {
             case 'civic':
-                return new Car(4, 'V6', 'grey');
+                return new Car(4, 'V6', 'grey', 'civic');
             case 'honda':
-                return new Car(2, 'V4', 'red');
+                return new Car(2, 'V4', 'red', 'honda');
         }
     }
 }
 
-const factory = new carFactory();
-const honda = factory.createCar('honda');
+class Suv {
+    constructor(doors, engine, color) {
+        this.doors = doors;
+        this.engine = engine;
+        this.color = color;
+    }
+}
 
-console.log(honda);
+class SuvFactory {
+    createCar(type) {
+        switch (type) {
+            case 'cx5':
+                return new Car(4, 'V6', 'grey', 'cx5');
+            case 'santa fe':
+                return new Car(2, 'V4', 'red', 'santa fe');
+        }
+    }
+}
+
+const carFactory = new CarFactory();
+const suvFactory = new SuvFactory();
+
+const autoManufacturer = (type, model) => {
+    switch (type) {
+        case 'car':
+            return carFactory.createCar(model);
+        case 'suv':
+            return suvFactory.createCar(model);
+    }
+}
+
+const cx5 = autoManufacturer('suv', 'cx5');
+
+console.log(cx5);
